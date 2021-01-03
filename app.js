43e01ea1a5982dd5a4eb4b388cb356a561d174f2
rohanC-dev <rohan.chopra241@gmail.com>
2021-01-03 11:20:27 -0500

    first version

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact");
+});
+
+app.get("/gallery", function(req, res){
+    res.render("gallery");
+});
+
+
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
+    console.log("server started");
+});
