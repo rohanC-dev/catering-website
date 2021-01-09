965a1b75a7c4d4e5b869421a000eb602c31b1989
rohanC-dev <rohan.chopra241@gmail.com>
2021-01-09 17:23:43 -0500

    modified some things from the last version, still v1.0

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
-});
-
 app.get("/contact", function(req, res){
     res.render("contact");
 });
